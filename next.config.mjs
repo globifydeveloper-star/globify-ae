4991ed98a7cca2197ebe5d4896eef47e9f3e7e6c
Rahulk2396 <rahulvidx0696@gmail.com>
2026-04-02 19:11:07 +0530

    added export for ot foldergeneration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: { unoptimized: true } // Standard hosting cannot run Next.js server-side image optimization
 };
 
