87203a64442dfad56243ae5ec11eabb45ea8e623
Rahulk2396 <rahulvidx0696@gmail.com>
2026-04-03 14:05:53 +0530

    update favicon for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true } // Standard hosting cannot run Next.js server-side image optimization
 };
 
 export default nextConfig;
